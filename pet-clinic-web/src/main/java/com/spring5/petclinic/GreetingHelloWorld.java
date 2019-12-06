bbd30c2b6635f254b28ef3d758af29dce8fd3db0
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-06 03:49:44 -0500

    Update GreetingHelloWorld.java

--- pet-clinic-web/src/main/java/com/spring5/petclinic/GreetingHelloWorld.java
+++ pet-clinic-web/src/main/java/com/spring5/petclinic/GreetingHelloWorld.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingHelloWorld {
+    int count = 0;
     @GetMapping("/")
     public String sayHello(){
+        System.err.println(" Count = " + count++ + " ############ TESTING ###############");
         return "Spring Works Well!!";
     }
 }
