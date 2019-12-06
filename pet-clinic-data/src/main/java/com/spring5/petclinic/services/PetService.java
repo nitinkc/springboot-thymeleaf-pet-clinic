9cd4233a91af466d13bde0c5dd64b78a15654ea6
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-06 09:02:23 -0500

    Removed repeated code using CrudService Interface

--- pet-clinic-data/src/main/java/com/spring5/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/spring5/petclinic/services/PetService.java
@@ -1,12 +1,6 @@
 package com.spring5.petclinic.services;
 
-import com.spring5.petclinic.model.Owner;
 import com.spring5.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
