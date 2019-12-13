ef3ef43d41b07787d7300ef1d0fa5e8a8248978f
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-12 20:16:47 -0500

    Kept the DB related ID values into a base entiry. Update date, create date can also be put

--- pet-clinic-data/src/main/java/com/spring5/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/spring5/petclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package com.spring5.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-    //Recommendation of Hibernate to use Box Type
-    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Compatible between h2 and MySQL
+    private Long id;    //Recommendation of Hibernate to use Box Type
 
     public Long getId() {
         return id;
